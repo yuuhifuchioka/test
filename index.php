803feafd6861be51ae469800d1db0aa8ef093359
yuuhifuchioka <69574777+yuuhifuchioka@users.noreply.github.com>
2020-08-13 17:56:08 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $base64 = "SUkqANatAAAAgLJZAQCAslkBAICyWQEAgLJZAQCAslkBAICyWQEAgLJZAQCAslkBAICyW
 
 $imageBlob = base64_decode($base64);
 $image64 = base64_encode($imageBlob);
-//$imagick = new Imagick();
+$imagick = new Imagick();
 
 $arr["base64"] = $image64;
 print json_encode($arr, JSON_PRETTY_PRINT);
